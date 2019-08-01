bc5b3a551ba0957916f517f318e9c5b4a85c3233
ayerest <irisiseve@gmail.com>
2019-07-31 21:59:30 -0700

    finished guessing cli lab

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,34 @@
 # Code your solution here!
+
+def run_guessing_game
+    random_num = (1..6).to_a.sample
+    # puts "Guess a number between 1 and 6"
+    user_guess = user_guess = gets.chomp
+    if user_guess.downcase == "exit"
+        puts "Goodbye!"
+    elsif user_guess.to_i == random_num
+        puts "You guessed the correct number!"
+    else
+        puts "Sorry! The computer guessed #{random_num}."
+    end
+end
+
+#helper method to get a random number between 1 and 6
+# def get_random_num
+#     (1..6).to_a.sample
+# end
+
+# def prompt_and_get_user_guess
+#     puts "Guess a number between 1 and 6"
+#     user_guess = gets.chomp
+# end
+
+# def check_guess(user_guess, random_num)
+#     if user_guess.downcase == "exit"
+#         puts "Goodbye!"
+#     elsif user_guess.to_i == random_num
+#         puts "You guessed the correct number!"
+#     else
+#         puts "Sorry! The computer guessed #{random_num}."
+#     end
+# end
